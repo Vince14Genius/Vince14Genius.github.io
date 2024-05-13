fa054691f29a306631f25bc5b1c4c9cfce1d6a20
Vince14Genius <vince14genius@icloud.com>
2024-05-12 21:01:49 -0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import React from 'react';
+import 'normalize.css';
 import logo from './logo.svg';
 import 'react-router-dom';
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main id="main-content" className="centered" lang="en">
+          <div className="main-width-wrapper">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+        </main>
+        <Footer/>
       </Router>
     </div>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="exterior-width-wrapper">
+        <hr className="compatibility-hidden"/>
+        <hr className="footer-hr"/>  
+        <p className="footer-normal compact-line-height">
+          Contact by email at <a className="footer-link" href="mailto:vince14genius@protonmail.com">vince14genius@protonmail.com</a>.
+        </p>
+        <p className="footer-normal compact-line-height">
+          <Link to="/site-map" className="footer-link">Site Map</Link> ・ 
+          <Link to="/privacy-policy" className="footer-link">Privacy Policy</Link> ・ 
+          <Link to="/support" className="footer-link">Support</Link></p>
+        <hr className="footer-hr"/>
+        <p className="footer-light compact-line-height"><span className="footer-normal">Copyright</span> © Vince14Genius (Vincent C.)</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
