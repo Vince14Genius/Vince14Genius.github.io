9254c48e2433d73aaf3586b26de493049ccebfbc
Vince14Genius <vince14genius@icloud.com>
2024-05-19 16:23:29 -0700

    projects initial

--- src/App.tsx
+++ src/App.tsx
@@ -11,85 +11,17 @@ import githubLogo from './images/socialmedia-github.svg';
 import instagramLogo from './images/socialmedia-instagram.svg';
 import youtubeLogo from './images/socialmedia-youtube.svg';
 
+import Index from './pages';
+import Projects from './pages/projects';
+
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
         <Routes>
-          <Route index element={
-            <main id="main-content" lang="en">
-              <div className="main-width-wrapper">
-                <section className="text-hello nav-aligned-width-wrapper">
-                  <p className="text-fancy text-hello">Welcome</p>
-                  <p className="text-hello">to my portfolio.</p>
-                </section>
-                <section className="social-media-section nav-aligned-width-wrapper noselect">
-                  <h2 className="compatibility-hidden">Social Media</h2>
-                  <p>
-                    <a className="social-media-link" href="https://github.com/Vince14Genius">
-                      <img alt="GitHub" src={githubLogo} />
-                    </a>
-                    
-                    <a className="social-media-link" href="https://www.instagram.com/vince14genius_0/">
-                      <img alt="Instagram" src={instagramLogo} />
-                    </a>
-                    
-                    <a className="social-media-link" href="https://youtube.com/@vince14genius">
-                      <img alt="YouTube" src={youtubeLogo} />
-                    </a>
-                  </p>
-                </section>
-                <section className="text-hello nav-aligned-width-wrapper">
-                  <hr className="standard-hr"/>
-                </section>
-                <section className="text-hello nav-aligned-width-wrapper">
-                  <h2 className="text-fancy text-hello">Languages</h2>
-                  <div className="language-chart">
-                    <div className="language-chart-header-column language-proficiency-native">Native</div>
-                    <div className="language-chart-data-column">
-                      <span className="language-tag">English</span>
-                      <span className="language-tag">中文</span>
-                    </div>
-                    <div className="language-chart-header-column language-proficiency-near-native">Near-Native</div>
-                    <div className="language-chart-data-column">
-                      <span className="language-tag">日本語</span>
-                    </div>
-                    <div className="language-chart-header-column language-proficiency-fluent">Fluent</div>
-                    <div className="language-chart-data-column">
-                    </div>
-                    <div className="language-chart-header-column language-proficiency-conversational">Conversational</div>
-                    <div className="language-chart-data-column">
-                      <span className="language-tag">Esperanto</span>
-                      <span className="language-tag">Español</span>
-                      <span className="language-tag">Français</span>
-                    </div>
-                    <div className="language-chart-header-column language-proficiency-beginner">Beginner</div>
-                    <div className="language-proficiency-secondary-tier language-chart-data-column">
-                      <span className="language-tag">Lingua Latīna</span>
-                      <span className="language-tag">한국어</span>
-                      <span className="language-tag">Ελληνικά</span>
-                      <span className="language-tag">Türkçe</span>
-                      <span className="language-tag">Bahasa Indonesia</span>
-                    </div>
-                  </div>
-                </section>
-                <section className="text-hello nav-aligned-width-wrapper">
-                  <hr className="standard-hr"/>
-                </section>
-                <section className="text-hello nav-aligned-width-wrapper">
-                  <p className="text-fancy text-hello">Text padding</p>
-                  <p className="text-hello">Text padding</p>
-                  <p className="text-fancy text-hello">Text padding</p>
-                  <p className="text-hello">Text padding</p>
-                  <p className="text-fancy text-hello">Text padding</p>
-                  <p className="text-hello">Text padding</p>
-                  <p className="text-fancy text-hello">Text padding</p>
-                  <p className="text-hello">Text padding</p>
-                </section>
-              </div>
-            </main>
-          } />
+          <Route index element={<Index />} />
+          <Route path="projects" element={<Projects />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/projects.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      <header className="local-nav">
+        <h1 className="nav-aligned-width-wrapper">Projects</h1>
+      </header>
+      <main id="main-content" lang="en">
+        <div className="main-width-wrapper">
+          <section className="text-hello nav-aligned-width-wrapper">
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+            <p className="text-fancy text-hello">Text padding</p>
+            <p className="text-hello">Text padding</p>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Projects;
