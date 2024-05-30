d8e2cf39dd596dba934c0c07154ab4fc2f0d715a
Vince14Genius <vince14genius@icloud.com>
2024-05-29 21:39:02 -0700

    various additions to the projects page

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -8,6 +8,7 @@ import appStoreBadge from '../../images/app-store-badge-black.svg';
 import ipaKeyboardIcon from './images/ipa-keyboard-icon.webp';
 import checkSplitterIcon from './images/check-splitter-icon.webp';
 import dynamicDungeonIcon from './images/dynamic-dungeon-icon.webp';
+import tilesOfArenasIcon from './images/tiles-of-arenas-icon.webp';
 
 function Projects() {
   return (
@@ -34,37 +35,18 @@ function Projects() {
                 </a>
                 <p className="project-item-info-bar">
                   <span className="highlighted-info-tags">
-                    <span>Web</span>
+                    <span className="project-type-header">Website</span>
                   </span>
-                  <span className="language-info-tags">
-                    <span>TypeScript</span>
-                    <span>HTML/CSS</span>
+                  <span className="platform-info-tags">
+                    <span>Web</span>
                   </span>
                 </p>
                 <p className="project-item-tags">
+                  <span>HTML/CSS</span>
+                  <span>TypeScript</span>
                   <span>React</span>
                   <span>npm</span>
                 </p>
-              </section>
-              <section className="projects-grid-item">
-              </section>
-              <section className="projects-grid-item">
-              </section>
-              <section className="projects-grid-item">
-              </section>
-            </div>
-          </section>
-          <section className="text-hello nav-aligned-width-wrapper">
-            <hr className="standard-hr" />
-          </section>
-          <section>
-            <h2 className="text-fancy">Actively work in progress</h2>
-            <div className="projects-grid">
-              <section className="projects-grid-item">
-              </section>
-              <section className="projects-grid-item">
-              </section>
-              <section className="projects-grid-item">
               </section>
             </div>
           </section>
@@ -88,19 +70,39 @@ function Projects() {
                 </a>
                 <p className="project-item-info-bar">
                   <span className="highlighted-info-tags">
-                    <span>App</span>
+                    <span className="project-type-header">App</span>
                   </span>
-                  <span className="language-info-tags">
-                    <span>Swift</span>
+                  <span className="platform-info-tags">
+                    <span>iOS</span>
                   </span>
                 </p>
                 <p className="project-item-tags">
-                  <span>iOS</span>
+                  <span>Swift</span>
                   <span>UIKit</span>
                   <span>SwiftUI</span>
+                  <span>Unicode</span>
                 </p>
               </section>
+            </div>
+          </section>
+          <section className="text-hello nav-aligned-width-wrapper">
+            <hr className="standard-hr" />
+          </section>
+          <section>
+            <h2 className="text-fancy">Work in progress</h2>
+            <div className="projects-grid">
               <section className="projects-grid-item">
+                <div className="project-title-description">
+                  <h3>Solids</h3>
+                  <p>???</p>
+                </div>
+                <div className="project-icon placeholder-icon" />
+              </section>
+              <section className="projects-grid-item">
+                <div className="project-icon placeholder-icon" />
+              </section>
+              <section className="projects-grid-item">
+                <div className="project-icon placeholder-icon" />
               </section>
             </div>
           </section>
@@ -124,18 +126,92 @@ function Projects() {
                 </a>
                 <p className="project-item-info-bar">
                   <span className="highlighted-info-tags">
-                    <span>App</span>
+                    <span className="project-type-header">App</span>
                   </span>
-                  <span className="language-info-tags">
-                    <span>Swift</span>
+                  <span className="platform-info-tags">
+                    <span>iOS</span>
+                    <span>macOS</span>
                   </span>
                 </p>
                 <p className="project-item-tags">
-                  <span>iOS</span>
-                  <span>macOS</span>
+                  <span>Swift</span>
                   <span>SwiftUI</span>
                   <span>AI/ML</span>
                   <span>Computer Vision</span>
+                </p>
+              </section>
+              <section className="projects-grid-item">
+                <div className="project-title-description">
+                  <h3>Tiles of Arenas</h3>
+                  <p>Board-game style turn-based multiplayer strategy game</p>
+                </div>
+                <img className="project-icon ios-app-icon" alt="app-icon" src={tilesOfArenasIcon} />
+                <p className="project-item-info-bar">
+                  <span className="highlighted-info-tags">
+                    <span className="project-type-header">Game</span>
+                    <span>WWDC 23</span>
+                  </span>
+                  <span className="platform-info-tags">
+                    <span>iOS</span>
+                    <span>macOS</span>
+                    <span>Web</span>
+                  </span>
+                </p>
+                <p className="project-item-tags">
+                  <span>Swift</span>
+                  <span>SwiftUI</span>
+                  <span>Game Design</span>
+                  <span>JavaScript</span>
+                </p>
+                <a className="project-github-link" href="https://github.com/Vince14Genius/Tiles-of-Arenas-SwiftUI">
+                  <img alt="GitHub" src={githubLogo} />
+                </a>
+              </section>
+              <section className="projects-grid-item">
+                <div className="project-title-description">
+                  <h3>NeoPixel Wotagei Glowsticks</h3>
+                  <p>NeoPixelで出来たヲタ芸用のペンライト</p>
+                </div>
+                <div className="project-icon placeholder-icon" />
+                <p className="project-item-info-bar">
+                  <span className="highlighted-info-tags">
+                    <span className="project-type-header">Hardware</span>
+                  </span>
+                  <span className="platform-info-tags">
+                    <span>IRL</span>
+                  </span>
+                </p>
+                <p className="project-item-tags">
+                  <span>Python</span>
+                  <span>CircuitPython</span>
+                  <span>Arduino</span>
+                  <span>Electrical Engineering</span>
+                </p>
+              </section>
+              <section className="projects-grid-item">
+                <div className="project-title-description">
+                  <h3>Wotagei x Rhythm Game Simulator</h3>
+                  <p>-</p>
+                </div>
+                <div className="project-icon placeholder-icon" />
+                <a className="project-github-link" href="https://github.com/Vince14Genius/WWDC20-Wotagei-x-Music-Game">
+                  <img alt="GitHub" src={githubLogo} />
+                </a>
+                <p className="project-item-info-bar">
+                  <span className="highlighted-info-tags">
+                    <span className="project-type-header">Experience</span>
+                    <span>WWDC 20</span>
+                  </span>
+                  <span className="platform-info-tags">
+                    <span>Swift Playgrounds</span>
+                  </span>
+                </p>
+                <p className="project-item-tags">
+                  <span>Swift</span>
+                  <span>SpriteKit</span>
+                  <span>Visual Design</span>
+                  <span>VFX Design</span>
+                  <span>Computer Simulation</span>
                 </p>
               </section>
               <section className="projects-grid-item">
@@ -152,37 +228,37 @@ function Projects() {
                 </a>
                 <p className="project-item-info-bar">
                   <span className="highlighted-info-tags">
-                    <span>Game</span>
+                    <span className="project-type-header">Game</span>
                     <span>WWDC 17</span>
                   </span>
-                  <span className="language-info-tags">
-                    <span>Swift</span>
+                  <span className="platform-info-tags">
+                    <span>iOS</span>
+                    <span>macOS</span>
                   </span>
                 </p>
                 <p className="project-item-tags">
-                  <span>iOS</span>
-                  <span>macOS</span>
+                  <span>Swift</span>
                   <span>SpriteKit</span>
-                  <span>WWDC</span>
+                  <span>Game Design</span>
                 </p>
               </section>
               <section className="projects-grid-item">
+                <div className="project-icon placeholder-icon" />
               </section>
               <section className="projects-grid-item">
+                <div className="project-icon placeholder-icon" />
               </section>
               <section className="projects-grid-item">
+                <div className="project-icon placeholder-icon" />
               </section>
               <section className="projects-grid-item">
+                <div className="project-icon placeholder-icon" />
               </section>
               <section className="projects-grid-item">
+                <div className="project-icon placeholder-icon" />
               </section>
               <section className="projects-grid-item">
-              </section>
-              <section className="projects-grid-item">
-              </section>
-              <section className="projects-grid-item">
-              </section>
-              <section className="projects-grid-item">
+                <div className="project-icon placeholder-icon" />
               </section>
             </div>
           </section>
